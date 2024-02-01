6936d7d0a8add49159dd98a7e87853acecea5246
Nikita Pivovarov <nikita.pivovarov2003@gmail.com>
2024-02-01 23:11:08 +0300

    initial project setup

---
+++ SystemsTest/SystemsTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SystemsTest
+//
+//  Created by Никита Пивоваров on 01.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SystemsTest/SystemsTest/SystemsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemsTestApp.swift
+//  SystemsTest
+//
+//  Created by Никита Пивоваров on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SystemsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
